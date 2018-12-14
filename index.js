5eb07ff8b490450dc69b9efdbed4a8191fd0a3d3
ugiriwabo <ugiraconsole@gmail.com>
2018-12-14 09:02:42 +0200

    add some code in javascript change some word

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 var add = function(number1, number2) {
     return number1 + number2;
     };
-    var number1 = parseInt(prompt("Enter a number:"));
-    var number2 = parseInt(prompt("Enter another number:"));
+    var number1 = parseInt(prompt("Enter a first number for addition:"));
+    var number2 = parseInt(prompt("Enter second number for addition:"));
     alert(add(number1, number2));
  
  
 var substract = function(number1, number2) {
     return number1 - number2;
     }
-    var number1 = parseInt(prompt("enter a number:"));
-    var nuumber2 = parseInt(prompt("enter another number"));
+    var number1 = parseInt(prompt("Enter a first number for substraction:"));
+    var nuumber2 = parseInt(prompt("Enter second number for substraction:"));
     alert(substract(number1, number2));
      
 var division = function(number1, number2) {
     return number1 / number2
     }
-    var number1 = parseInt(prompt("enter a number"));
-    var number2 = parseInt(prompt("enter another number"));
+    var number1 = parseInt(prompt("Enter a first number for division:"));
+    var number2 = parseInt(prompt("Enter second number for division:"));
     alert(division(number1, number2));
      
      
 var multiplication = function(number1, number2) {
     return number1 * number2
     }
-    var number1 = parseInt(prompt("enter a number"));
-    var number2 = parseInt(prompt("enter another number"));
+    var number1 = parseInt(prompt("Enter a first number for multiplication"));
+    var number2 = parseInt(prompt("Enter second number for multiplication"));
     alert(multiplication(number1, number2));
-     
-        
+    
+    
