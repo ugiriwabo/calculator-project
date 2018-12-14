0a998af60dbb921bc0f9a7588063d0d18ad97847
ugiriwabo <ugiraconsole@gmail.com>
2018-12-14 08:37:53 +0200

    add some code in javascript for Division

--- index.js
+++ index.js
@@ -13,4 +13,12 @@ var substract = function(number1, number2) {
     var nuumber2 = parseInt(prompt("enter another number"));
     alert(substract(number1, number2));
      
+var division = function(number1, number2) {
+    return number1 / number2
+    }
+    var number1 = parseInt(prompt("enter a number"));
+    var number2 = parseInt(prompt("enter another number"));
+    alert(division(number1, number2));
+     
+     
         
